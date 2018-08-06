8de8fe73c874c681ec1abc11dd5fbd2295cb0f2c
chen72 <16046469@myrp.edu.sg>
2018-08-06 15:30:38 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/webservices/demonavigation/NewActivity.java
@@ -0,0 +1,32 @@
+package sg.edu.rp.webservices.demonavigation;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class NewActivity extends AppCompatActivity {
+
+    ActionBar ab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //action
+                Toast.makeText(getBaseContext(),"hi",Toast.LENGTH_SHORT).show();;
+            }
+        });
+
+    }
+}
